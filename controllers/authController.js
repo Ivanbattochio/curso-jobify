7bc5bc124cbc12a987d33c70e06f27fd7485f930
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2022-05-12 21:54:01 -0300

    feat: token authentication middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,54 +1,55 @@
-import User from "../models/User.js";
-import { StatusCodes } from "http-status-codes";
-import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import User from "../models/User.js"
+import { StatusCodes } from "http-status-codes"
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js"
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    throw new BadRequestError("please provide all values");
-  }
+	const { name, email, password } = req.body
+	if (!name || !email || !password) {
+		throw new BadRequestError("please provide all values")
+	}
 
-  const userAlreadyExists = await User.findOne({ email });
+	const userAlreadyExists = await User.findOne({ email })
 
-  if (userAlreadyExists) {
-    throw new BadRequestError("email already in use");
-  }
-  const user = await User.create({ name, email, password });
-  const token = user.createJWT();
-  res.status(StatusCodes.CREATED).json({
-    user: {
-      email: user.email,
-      lastName: user.lastName,
-      location: user.location,
-      name: user.name,
-    },
-    token,
-    location: user.location,
-  });
-};
+	if (userAlreadyExists) {
+		throw new BadRequestError("email already in use")
+	}
+	const user = await User.create({ name, email, password })
+	const response = user.createJWT()
+	const token = response.token
+	res.status(StatusCodes.CREATED).json({
+		user: {
+			email: user.email,
+			lastName: user.lastName,
+			location: user.location,
+			name: user.name,
+		},
+		token,
+		location: user.location,
+	})
+}
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    throw new BadRequestError("Please provide all values!");
-  }
-  const user = await User.findOne({ email }).select("+password -_id -__v");
+	const { email, password } = req.body
+	if (!email || !password) {
+		throw new BadRequestError("Please provide all values!")
+	}
+	const user = await User.findOne({ email }).select("+password -__v")
 
-  if (!user) {
-    throw new UnAuthenticatedError("Invalid credentials");
-  }
-  const checkPassword = await user.comparePassword(password);
+	if (!user) {
+		throw new UnAuthenticatedError("Invalid credentials")
+	}
+	const checkPassword = await user.comparePassword(password)
 
-  if (!checkPassword) {
-    throw new UnAuthenticatedError("Invalid credentials");
-  }
-  const token = user.createJWT();
-  user.password = undefined;
-  res.status(StatusCodes.OK).json({ user, token, location: user.location });
-};
+	if (!checkPassword) {
+		throw new UnAuthenticatedError("Invalid credentials")
+	}
+	const token = user.createJWT()
+	user.password = undefined
+	res.status(StatusCodes.OK).json({ user, token, location: user.location })
+}
 
 const updateUser = (req, res) => {
-  res.send("updateUser");
-};
+	res.send("updateUser")
+}
 
-export { register, login, updateUser };
+export { register, login, updateUser }
